9f67d61347b13a6a7c58ceaece045e5b4bea4656
Zac <90797142+StreetCan@users.noreply.github.com>
2025-10-28 22:37:59 +0200

    Update link to rick roll

--- src/components/made-with-dyad.tsx
+++ src/components/made-with-dyad.tsx
@@ -2,13 +2,14 @@ export const MadeWithDyad = () => {
   return (
     <div className="p-4 text-center">
       <a
-        href="https://www.dyad.sh/"
+        href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
         target="_blank"
         rel="noopener noreferrer"
         className="text-sm text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
       >
-        Made with Dyad
+        if you can read this, you shouldn't be able to...
       </a>
     </div>
   );
 };
+

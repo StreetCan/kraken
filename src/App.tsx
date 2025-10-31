5489e55969ad5c1ed438b32386d348f369e24545
Zac <90797142+StreetCan@users.noreply.github.com>
2025-10-31 22:39:37 +0200

    1.0 Release

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ const Header: React.FC = () => {
       <div className="max-w-[1700px] mx-auto px-4 py-3 flex items-center justify-between">
         <div className="flex items-center gap-4">
           <Link to="/" className="text-lg font-semibold">
-            Kraken 0.9
+            Kraken 1.0
           </Link>
         </div>
 
